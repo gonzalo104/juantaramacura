90c03e03abc3fe28a8009288e0b19577bc3e130b
gonzalo104 <gonzalozavalamendoza@gmail.com>
2018-07-06 16:38:53 -0500

    chat

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app  = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.use(express.static(__dirname + '/public'));
 app.listen(port, () => console.log(`Escuchando peticiones en el puerto ${port}`)); 
